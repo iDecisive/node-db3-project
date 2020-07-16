63d1129a5a15cef86f7ced29b074cb990521d611
Daniel Johnson <danielaugustusjohnson@gmail.com>
2020-07-16 02:55:03 -0400

    add works

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,18 +1,24 @@
 const db = require('../data/schemesConfig.js');
 
 const find = (_) => {
-    return db('schemes');
+	return db('schemes');
 };
 
 const findById = (id) => {
-    return db('schemes').where({ id }).first();
+	return db('schemes').where({ id }).first();
 };
 
 const findSteps = (id) => {
-    return db('steps').where({ id }); //Not done
+	return db('steps').where({ id }); //Not done
 };
 
-const add = (obj) => {};
+const add = (obj) => {
+	return db('schemes').insert(obj).then(returned => {
+        return returned;
+    }).catch(err => {
+        return err
+    });
+};
 
 const update = (changes, id) => {};
 
