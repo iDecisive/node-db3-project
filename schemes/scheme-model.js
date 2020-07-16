0b6c2fcfeaa1e1f210d552cfbef4194cb58ee50f
Daniel Johnson <danielaugustusjohnson@gmail.com>
2020-07-16 02:42:10 -0400

    Finished queries.sql file - find() and findById() both work with sqlite db

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,1 +1,28 @@
- 
+const db = require('../data/schemesConfig.js');
+
+const find = (_) => {
+    return db('schemes');
+};
+
+const findById = (id) => {
+    return db('schemes').where({ id }).first();
+};
+
+const findSteps = (id) => {
+    return db('steps').where({ id }); //Not done
+};
+
+const add = (obj) => {};
+
+const update = (changes, id) => {};
+
+const remove = (id) => {};
+
+module.exports = {
+	find,
+	findById,
+	findSteps,
+	add,
+	update,
+	remove,
+};
